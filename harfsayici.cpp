c7fb9a177118868fe7c1869d545c8c8c2a26ee7c
Çağan <cagangdk6@hotmail.com>
2020-04-03 01:47:56 +0300

    Add files via upload

---
+++ harfsayici.cpp
@@ -0,0 +1,36 @@
+
+#include<iostream>
+#include<locale>
+
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	char a[20] = " ";
+	char b=' ';
+	int sayac = 0;
+	char y;
+	cout << "Bir kelime giriniz(max 20 harf): " << a;
+	cin >> a;
+	cout<<"Hangi harfi saymasýný istediðinizi yazýn: ";
+	cin>> b;
+	int t=b;
+	if(t>96 && t<123){
+		 y=t-32;
+	}
+	else if(t>64 && t<91){
+		 y=t+32;
+	}
+	for (int i = 0; i < 20; i++) {
+		if (a[i] == b || a[i] == y) {
+			sayac = sayac + 1;
+
+		}
+	}
+	cout << "Kelimenizde " << sayac << " adet 'a' harfi vardýr.";
+
+
+
+	return 0;
+}
