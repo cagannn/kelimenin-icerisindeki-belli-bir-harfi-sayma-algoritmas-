47ec971bb63d15eefb90781c1ff4b8f5643c8442
Çağan <cagangdk6@hotmail.com>
2020-04-11 05:33:40 +0300

    Create harfsayici2.cpp

---
+++ harfsayici2.cpp
@@ -0,0 +1,21 @@
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	char a[20]=" ";
+	char sayilacakharf;
+	int sayac = 0;
+	cout << "Hangi kelimenin üzerine sayılacak: ";
+	cin >> a;
+	cout <<endl<< "Sayılmasını istediğiniz harfi giriniz: ";
+	cin >> sayilacakharf;
+	for (char item:a) {
+
+		if (item == sayilacakharf) {
+			sayac++;
+		}
+		
+		
+	}
+	cout << sayac;
+
+	return 0;
+}
